1509815d509a6242f625fc5134abcfae5902a51c
wu7 <wu7czz@gmail.com>
2019-04-21 16:47:23 +0800

    chang2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import calendar from './calendar.vue';
-calendar.install = Vue => Vue.component(calendar.name, calendar);
-export default calendar;
+calendar.install = Vue => Vue.component(calendar.name, calendar)
+export default calendar
